77c1c434f5051f68be3d241821379518a679f8da
João Vitor Penha <joao.penha.martins47@gmail.com>
2026-09-03 16:25:21 -0300

    ex3trycatch

--- src/ListaString.java
+++ src/ListaString.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 void main() {
     /*
@@ -11,7 +12,24 @@ void main() {
     System.out.println("Digite uma frase");
     String frase = leitura.nextLine();
     //ListaString.verifica_vogais(frase);
+
+    /*
+    2. Desenvolva um programa gerador de usernames. O programa deve gerar e apresentar 5 usernames
+    aleatórios utilizando uma sequência de consoante + vogal com tamanhos variados de 4, 6 ou 8
+    caracteres. Exemplos de possíveis usernames gerados: *bexa*, *Rinato*, *maruko*, *tovira*,
+    *kitona*.
+    Dica:
+     Math.random() retorna um valor aleatório do tipo real entre 0 e 1. Se você quiser gerar um
+    número aleatório entre 1 e 25, faça o seguinte comando: int n=(int)(Math.random()*25)+1
+    */
     ListaString.gerador_de_usernames();
+    /*
+    3. Faça o metodo estático LerInt() em Java que recebe como parâmetro um título e uma mensagem. O
+    metodo deve abrir uma caixa de entrada no estilo JOptionPane.showInputDialog(...) com o título e a
+    mensagem recebidos como parâmetro e retornar na forma de um inteiro, o valor digitado pelo
+    usuário. Caso o usuário digite um valor inválido a função deve retornar o valor zero. Faça também uma
+    versão dessa função para a leitura de valores reais.
+    */
 }
 
 public class ListaString{
@@ -69,5 +87,15 @@ public class ListaString{
         }
 
     }
+    public static int ler_painel(String titulo, String mensagem){
+            try {
+                int valor = Integer.parseInt(JOptionPane.showInputDialog(null, mensagem, titulo, JOptionPane.QUESTION_MESSAGE));
+                return valor;
+            }
+            catch(Exception e) {
+                return 0;
+            }
 
-}
+        }
+    }
+
